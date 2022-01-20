30102dcff9441a897aac35d57de5815ced2889e4
Devin Cooley <devincooley.dev@gmail.com>
2022-01-19 20:58:21 -0800

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
             })}
         </div>
         <hr />
-        <p className="footer__text">© {new Date().getFullYear()} - Devin Cooley</p>
+        <p className="footer__text">{new Date().getFullYear()} - Devin Cooley</p>
         {isEnabled && <GithubButtons />}
       </Container>
     </footer>
