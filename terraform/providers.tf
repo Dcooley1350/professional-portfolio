2b0a38a40c8c861fc1ef6cd802d7422061f91b27
Devin Cooley <devincooley.dev@gmail.com>
2022-03-21 09:36:18 -0700

    Add terraform files for hosting

---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.1.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "cooleytech-terraform-state"
+    key    = "devincooley.dev/prod/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-west-2"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+domain_name = "devincooley.dev"
+bucket_name = "devincooley.dev"
+
+common_tags = {
+  Project = "devincooley.dev"
+}
